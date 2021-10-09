54dff1ca2d1c80a7f6e4e5955bdf034b0953f07b
Michael Troger <dev@michaeltroger.com>
2021-10-10 01:12:15 +0200

    Add documentation

--- src/commonMain/kotlin/com/michaeltroger/latintocyrillic/LatinCyrillic.kt
+++ src/commonMain/kotlin/com/michaeltroger/latintocyrillic/LatinCyrillic.kt
@@ -2,9 +2,28 @@ package com.michaeltroger.latintocyrillic
 
 public interface LatinCyrillic {
 
+    /**
+     * Convert the given text from latin script to cyrillic script
+     */
     public suspend fun latinToCyrillic(text: String): String
+
+    /**
+     * Convert the given text from cyrillic script to latin script
+     */
     public suspend fun cyrillicToLatin(text: String): String
+
+    /**
+     * Check if the given text is considered cyrillic
+     */
     public suspend fun isCyrillic(text: String): Boolean
+
+    /**
+     * The used alphabet in latin script
+     */
     public suspend fun getLatinAlphabet(): List<String>
+
+    /**
+     * The used alphabet in cyrillic script
+     */
     public suspend fun getCyrillicAlphabet(): List<String>
 }
--- src/commonMain/kotlin/com/michaeltroger/latintocyrillic/LatinCyrillicFactory.kt
+++ src/commonMain/kotlin/com/michaeltroger/latintocyrillic/LatinCyrillicFactory.kt
@@ -2,6 +2,20 @@ package com.michaeltroger.latintocyrillic
 
 public object LatinCyrillicFactory {
 
+    /**
+     * Use this method to choose one of the existing Latin/Cyrillic alphabets
+     * @param alphabet the [Alphabet] to initialize with
+     * @return the [LatinCyrillic] object initialized with the chosen alphabet
+     */
     public fun create(alphabet: Alphabet): LatinCyrillic = LatinCyrillicImpl(alphabet)
+
+    /**
+     * Use this method if you want to specify a custom Latin/Cyrillic alphabet
+     * @param latin 1 or 2 characters are allowed to describe a letter
+     * @param cyrillic  1 character is allowed to describe a letter
+     * Both [latin] and [cyrillic] lists need to be of same size.
+     * Index 0 of [latin] is associated with index 0 of [cyrillic], index 1 with index 1 and so on
+     * @return the [LatinCyrillic] object initialized with the custom alphabet
+     */
     public fun create(latin: List<String>, cyrillic: List<Char>): LatinCyrillic = LatinCyrillicImpl(latin = latin, cyrillic = cyrillic)
 }

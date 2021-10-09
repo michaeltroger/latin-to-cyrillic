04083d94a8f6ca8b3a599ee8181647e8b09ca605
Michael Troger <dev@michaeltroger.com>
2021-10-10 00:57:32 +0200

    Refactoring

--- src/commonMain/kotlin/com/michaeltroger/latintocyrillic/LatinCyrillicFactory.kt
+++ src/commonMain/kotlin/com/michaeltroger/latintocyrillic/LatinCyrillicFactory.kt
@@ -1,6 +1,6 @@
 package com.michaeltroger.latintocyrillic
 
-public object CyrillicFactory {
+public object LatinCyrillicFactory {
 
     public fun create(alphabet: Alphabet): LatinCyrillic = LatinCyrillicImpl(alphabet)
     public fun create(latin: List<String>, cyrillic: List<Char>): LatinCyrillic = LatinCyrillicImpl(latin = latin, cyrillic = cyrillic)

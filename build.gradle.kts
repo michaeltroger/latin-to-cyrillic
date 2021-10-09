2fe7d02750ac929956417a390668c6b0761b8482
Michael Troger <dev@michaeltroger.com>
2021-10-09 21:54:22 +0200

    Build native libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,20 @@ kotlin {
             }
         }
     }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+    nativeTarget.apply {
+        binaries {
+            sharedLib()
+            staticLib()
+        }
+    }
 
     sourceSets {
         val commonMain by getting {
@@ -43,6 +57,8 @@ kotlin {
         val jvmTest by getting
         val jsMain by getting
         val jsTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
     }
 
 }
---
+++ src/nativeTest/kotlin/com.michaeltroger.latintocyrillic/runTest.kt
@@ -0,0 +1,10 @@
+package com.michaeltroger.latintocyrillic
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
+
+actual val testCoroutineContext: CoroutineContext = Dispatchers.Main
+actual fun runBlockingTest(block: suspend CoroutineScope.() -> Unit) =
+ runBlocking { block() }

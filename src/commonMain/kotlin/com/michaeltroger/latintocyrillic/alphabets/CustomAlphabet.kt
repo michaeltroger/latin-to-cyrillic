c3ef5018060a1a129986879dbcb530fef2da8a7c
Michael Troger <dev@michaeltroger.com>
2021-10-10 12:27:00 +0200

    Validate in ctor

--- src/commonMain/kotlin/com/michaeltroger/latintocyrillic/alphabets/CustomAlphabet.kt
+++ src/commonMain/kotlin/com/michaeltroger/latintocyrillic/alphabets/CustomAlphabet.kt
@@ -1,24 +1,22 @@
 package com.michaeltroger.latintocyrillic.alphabets
 
-import kotlin.IllegalArgumentException
 
-/**
- * Both must be of same size and correctly sorted
- */
 internal class CustomAlphabet(latin: List<String>, cyrillic: List<Char>) : LatinCyrillicAlphabet() {
 
     init {
-        if (latin.size != cyrillic.size) {
-            throw IllegalArgumentException("Latin and Cyrillic alphabets must have same size. But have: Latin:${latin.size}/Cyrillic:${cyrillic.size}")
+        require(latin.size == cyrillic.size) {
+            "Latin and Cyrillic alphabets must have same size. But have: Latin:${latin.size}/Cyrillic:${cyrillic.size}"
+        }
+        latin.forEach {
+            require(it.isNotEmpty() && it.length < 3) {
+                "Only 1 or 2 latin characters allowed to describe a letter in alphabet. But were:${it.length}/Letter:${it}\""
+            }
         }
     }
 
     override val latinToCyrillicAlphabet: Map<String, String> by lazy {
         var i = 0
         latin.associateWith {
-            if (it.length > 2) {
-                throw IllegalArgumentException("Only up to 2 latin characters allowed to describe a letter in alphabet. But were: Latin:${it.length}/${it}")
-            }
             cyrillic[i++].toString()
         }
     }
